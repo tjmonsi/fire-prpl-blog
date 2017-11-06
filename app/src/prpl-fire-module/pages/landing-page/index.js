d4aaf412ee5fef53a6c3dc4d3be3a39e470cad95
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-11-06 17:19:40 +0800

    we have done our banner  yey!
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- app/src/prpl-fire-module/pages/landing-page/index.js
+++ app/src/prpl-fire-module/pages/landing-page/index.js
@@ -1,9 +1,20 @@
 import { Element } from '@polymer/polymer/polymer-element';
 import { customElements } from 'global/window';
 import './index.html';
+const bannerImage = 'https://firebasestorage.googleapis.com/v0/b/prpl-fire-blog.appspot.com/o/project-related-images%2Foriginal%2F4269497619_7232f17793_z%20(1).jpg?alt=media&token=62aeeca8-aca5-4b35-9aea-da32de9d7b89';
 
 class Page extends Element {
   static get is () { return 'landing-page'; }
+
+  static get properties () {
+    return {
+      bannerImage: {
+        type: String,
+        readOnly: true,
+        value: bannerImage
+      }
+    };
+  }
 }
 
 !customElements.get(Page.is)
