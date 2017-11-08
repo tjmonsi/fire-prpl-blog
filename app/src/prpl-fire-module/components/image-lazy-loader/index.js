0bd59ad234ff0faa8cc2d3fa5e105a877783c12e
aldrin bautista <adinbautista@gmail.com>
2017-11-08 11:28:15 +0800

    use transition event listener

--- app/src/prpl-fire-module/components/image-lazy-loader/index.js
+++ app/src/prpl-fire-module/components/image-lazy-loader/index.js
@@ -2,6 +2,21 @@ import { Element } from '@polymer/polymer/polymer-element';
 import { customElements } from 'global/window';
 import './index.html';
 
+const getTransitionEvent = (el) => {
+    const transitions = {
+        'transition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'MozTransition': 'Transitionend',
+        'WebkitTransition': 'webkitTransitionEnd'
+    };
+
+    for (var t in transitions) {
+        if (el.style[t] !== undefined) {
+            return transitions[t];
+        }
+    }
+};
+
 class Component extends Element {
   static get is () { return 'image-lazy-loader'; }
 
@@ -17,6 +32,15 @@ class Component extends Element {
   _loadImage (img) {
     const thumbnailImage = this.querySelector('img');
     const image = document.createElement('img');
+    const transition = getTransitionEvent(thumbnailImage);
+
+    const fn = () => {
+      thumbnailImage.removeEventListener(transition, fn);
+      this.removeChild(thumbnailImage);
+    };
+
+    thumbnailImage.addEventListener(transition, fn);
+
     image.src = img;
     image.onload = () => {
       this.appendChild(image);
